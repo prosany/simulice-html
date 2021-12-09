1f7fc8180e80c708a6c6f8c1e4537346f7299d4c
Mahabub Sunny <mahabubsany2@gmail.com>
2021-12-09 12:21:51 +0600

    HH

---
+++ javaScript/simulice.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#header-navbar").load("../components/HomePage/navBar/navbar.html");
+    $("#sign-in").load("../components/SignIn/SignIn.html");
+    $("#hero-section").load("../components/HomePage/heroSection/heroSection.html");
+    $("#find-job").load("../components/HomePage/findJob/findjob.html");
+    $("#find-job-detailed").load("../components/HomePage/findJobDetailed/findjobdetailed.html");
+    $("#show-details-results").load("../components/detailedSearchRes/OtherService/osDetailsSearchRes.html");
+
+    
+    $("#temp_worker").load("../components/TempWorker/profile/TempWorker.html");
+    $("#edit_profile").load("../components/companyProfile/EditCompanyProfile_50/edit-company-profile.html");
+    $("#user_settings").load("../components/companyProfile/UserSettings_52/user-settings.html");
+    $("#job_Ads").load("../components/employee/employeeJobSerarch_74/employeeJobPost.html");
+
+    $("#company_profile_40").load("../components/companyProfile/companyProfile_40/company_profile.html");
+    $("#company_Details_44").load("../components/companyProfile/companyDetails_44/companyDetails.html");
+});
